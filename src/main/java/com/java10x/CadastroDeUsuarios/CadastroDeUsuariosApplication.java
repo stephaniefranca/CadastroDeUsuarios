87a6091b207a9a2796434449cab2731db0eb6e6a
stephaniefranca <stephanie.dfranca@gmail.com>
2025-07-24 17:41:40 -0300

    first commit

---
+++ src/main/java/com/java10x/CadastroDeUsuarios/CadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.java10x.CadastroDeUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java10x/CadastroDeUsuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java10x.CadastroDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
