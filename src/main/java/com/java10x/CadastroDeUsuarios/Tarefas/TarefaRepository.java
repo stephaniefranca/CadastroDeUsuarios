27bc334a5c269f42fb287167c7c2d0b55d162348
stephaniefranca <stephanie.dfranca@gmail.com>
2025-07-28 17:17:36 -0300

    feat: Cria TarefaRepository e conecta ao JPA

---
+++ src/main/java/com/java10x/CadastroDeUsuarios/Tarefas/TarefaRepository.java
@@ -0,0 +1,6 @@
+package com.java10x.CadastroDeUsuarios.Tarefas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository <TarefaModel, Long>{
+}
