ded1289df2517320bc4f8e68d39fe28e435f1bb7
stephaniefranca <stephanie.dfranca@gmail.com>
2025-08-06 21:47:11 -0300

    feat: atualiza o controller com o mock de dados

--- src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
+++ src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.java10x.CadastroDeUsuarios.Usuarios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,37 @@ public class UsuarioController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Ola! Bem vindo!";
+    }
+
+    // CRUD
+
+    // Adicionar user (create)
+    @PostMapping("/criar")
+    public String criarUsuario(){
+        return "Usuario criado";
+    }
+
+    // Mostrar todos os user (read)
+    @GetMapping("/todos")
+    public String mostrarTodoUsuarios(){
+        return "Todos usuarios";
+    }
+
+    //Mostrar user por ID (read)
+    @GetMapping("/todosId")
+    public String mostrarTodoUsuariosPorId(){
+        return "Usuario Id";
+    }
+
+    // Alterar dados do user (update)
+    @PutMapping("/alterarId")
+    public String alterarUsuarioPorId(){
+        return "Alterar usuario po Id";
+    }
+
+    //Deletar user (delete)
+    @DeleteMapping("/deletarId")
+        public String deletarUsuarioPorId(){
+            return "Deletar usuario po Id";
     }
 }
