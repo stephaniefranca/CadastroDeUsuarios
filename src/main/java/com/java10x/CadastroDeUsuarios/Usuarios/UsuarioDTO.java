eac3e7d5a83b6a3b1f399f64b3df159cf0984b64
stephaniefranca <stephanie.dfranca@gmail.com>
2025-08-13 17:19:58 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no UsuarioMapper os caminhos entidade e DTO

--- src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioDTO.java
+++ src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioDTO.java
@@ -1,4 +1,29 @@
 package com.java10x.CadastroDeUsuarios.Usuarios;
 
+import com.java10x.CadastroDeUsuarios.Tarefas.TarefaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UsuarioDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String imgUrl;
+
+    private int idade;
+
+    private String rank;
+
+    private TarefaModel tarefas;
+
+
 }
--- src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioMapper.java
+++ src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioMapper.java
@@ -1,4 +1,37 @@
 package com.java10x.CadastroDeUsuarios.Usuarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+
+    public UsuarioModel map(UsuarioDTO usuarioDTO){
+        UsuarioModel usuarioModel = new UsuarioModel();
+
+        usuarioModel.setId(usuarioDTO.getId());
+        usuarioModel.setNome(usuarioDTO.getNome());
+        usuarioModel.setEmail(usuarioDTO.getEmail());
+        usuarioModel.setIdade(usuarioDTO.getIdade());
+        usuarioModel.setImgUrl(usuarioDTO.getImgUrl());
+        usuarioModel.setRank(usuarioDTO.getRank());
+        usuarioModel.setTarefas(usuarioDTO.getTarefas());
+
+        return usuarioModel;
+
+    }
+
+    public UsuarioDTO map(UsuarioModel usuarioModel){
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+
+        usuarioDTO.setId(usuarioDTO.getId());
+        usuarioDTO.setNome(usuarioDTO.getNome());
+        usuarioDTO.setEmail(usuarioDTO.getEmail());
+        usuarioDTO.setIdade(usuarioDTO.getIdade());
+        usuarioDTO.setImgUrl(usuarioDTO.getImgUrl());
+        usuarioDTO.setRank(usuarioDTO.getRank());
+        usuarioDTO.setTarefas(usuarioDTO.getTarefas());
+
+        return usuarioDTO;
+
+    }
 }
