e545a5250b1411a2ba8fdbe53678a7d8b5e8dbba
stephaniefranca <stephanie.dfranca@gmail.com>
2025-09-09 17:40:38 -0300

    doc: Adiciona Swagger paar documentação das rotas no controller de usuarios

--- src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
+++ src/main/java/com/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
@@ -1,5 +1,9 @@
 package com.java10x.CadastroDeUsuarios.Usuarios;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/boasvindas")
+    @Operation (summary = "Mensagem de Boas Vindas", description = "Essa rota retorna uma mensagem de boas vindas para quem acessar.")
     public String boasVindas(){
         return "Ola! Bem vindo!";
     }
@@ -25,6 +30,11 @@ public class UsuarioController {
 
     // Adicionar user (create)
     @PostMapping("/criar")
+    @Operation (summary = "Criar um novo usuario", description = "Essa rota cria um novo usuario e insere no banco de dados.")
+    @ApiResponses (value = {
+            @ApiResponse (responseCode = "201", description = "Usuario criado com sucesso"),
+            @ApiResponse (responseCode = "400", description = "Erro na criação do usuario")
+    })
     public ResponseEntity<String> criarUsuario(@RequestBody UsuarioDTO usuario){
         UsuarioDTO usuarioNovo = usuarioService.criarUsuario(usuario);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -40,6 +50,11 @@ public class UsuarioController {
 
     //Mostrar user por ID (read)
     @GetMapping("/listar/{id}")
+    @Operation (summary = "Lista o usuario por ID", description = "Essa rota lista um usuario pelo seu ID")
+    @ApiResponses (value = {
+            @ApiResponse (responseCode = "200", description = "Usuario encontrado com sucesso"),
+            @ApiResponse (responseCode = "404", description = "Usuario não encontrado")
+    })
     public ResponseEntity<?> listarUsuariosPorId(@PathVariable Long id){
         UsuarioDTO usuario = usuarioService.listarUsuariosPorId(id);
         if (usuario != null){
@@ -52,7 +67,16 @@ public class UsuarioController {
 
     // Alterar dados do user (update)
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado){
+    @Operation (summary = "Altera o usuario por ID", description = "Essa rota altera um usuario pelo seu ID")
+    @ApiResponses (value = {
+            @ApiResponse (responseCode = "200", description = "Usuario alterado com sucesso"),
+            @ApiResponse (responseCode = "404", description = "Usuario não encontrado, não é possivel alterar")
+    })
+    public ResponseEntity<?> atualizarUsuario(
+            @Parameter (description = "Usuario manda o ID pelo caminho da requisição")
+            @PathVariable Long id,
+            @Parameter (description = "Usuario manda os dados a serem atualizados no corpo da requisição")
+            @RequestBody UsuarioDTO usuarioAtualizado){
         try {
             usuarioService.atualizarUsuario(id, usuarioAtualizado);
             UsuarioDTO usuarioAtualizadoResponse = usuarioService.listarUsuariosPorId(id);
